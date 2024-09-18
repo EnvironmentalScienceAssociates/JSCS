421397ed6af3b0b7d45be16d524fa0990c19c4e2
Travis Hinkelman <thinkelman@esassoc.com>
2024-09-18 13:20:05 -0700

    Add prep_tc_meta

--- R/trap.R
+++ R/trap.R
@@ -17,3 +17,29 @@ get_tc <- function(table = c("meta", "fish", "tally", "stomach", "transport", "t
   delver::get_dataset_file(project_id = 73, dataset_id = 206, version = 1,
                            filename = tc_tables[[table]])
 }
+
+#' Prepare DELVE trap collection metadata
+#'
+#'
+#' @md
+#' @param tc_meta_raw     Unprocessed trap collection metadata
+#'
+#' @export
+#'
+
+prep_tc_meta <- function(tc_meta_raw){
+  tc_meta_raw |>
+    fix_names() |>
+    dplyr::mutate(start_datetime = prep_datetime(start_datetime),
+                  end_datetime = prep_datetime(end_datetime),
+                  trap_open_time = prep_datetime(trap_open_time),
+                  trap_closed_time = prep_datetime(trap_closed_time),
+                  trap_hours = as.numeric(difftime(end_datetime, start_datetime, units = "hours")),
+                  start_date = as.Date(start_datetime, tz = "America/Los_Angeles"),
+                  temp_c = ifelse(temp_c == 0, NA, temp_c),
+                  trap_status = from_canonical(trap_status)) |>
+    dplyr::filter(start_date >= min_tc_date) |>
+    dplyr::select(sample_id = sampleid, tc_meta_id = recordkey, obs_loc, date = start_date,
+                  datetime = start_datetime, end_datetime, trap_hours, trap_status, temp_c,
+                  trap_open_time, trap_closed_time)
+}
