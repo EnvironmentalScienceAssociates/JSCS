ebeda53e3783ec0cbadb467b5c83a0134be7c686
Travis Hinkelman <thinkelman@esassoc.com>
2024-09-29 16:16:22 -0700

    Expand fp and tp functions to include more fields

--- R/fish_processing.R
+++ R/fish_processing.R
@@ -26,7 +26,9 @@ get_fp <- function(table = c("main", "fish", "plus")){
 
 prep_fp_main <- function(fp_main_raw){
   fp_main_raw |>
-    dplyr::select(fp_main_id = `_record_id`, date, trap_check_number)
+    dplyr::rename(fp_main_id = `_record_id`) |>
+    dplyr::select(!dplyr::starts_with("_") & !dplyr::contains("photos")) |>
+    dplyr::relocate(dplyr::contains("id"), .after = dplyr::last_col())
 }
 
 #' Prepare Fulcrum fish measurements table
@@ -41,13 +43,12 @@ prep_fp_main <- function(fp_main_raw){
 
 prep_fp_fish <- function(fp_fish_raw, fp_main){
   fp_fish_raw |>
-    dplyr::select(fp_fish_id = `_child_record_id`, fp_main_id = `_parent_id`,
-                  species = species_measure, lifestage = lifestage_measure,
-                  measure_type, fl_mm = fork_length_mm, fish_viewer_id,
-                  mark_status = mark_status_measure, pit_tag_id, floy_tag_id,
-                  fish_condition = fish_condition_measure, stomach_sampled, disposition) |>
-    dplyr::mutate(count = 1) |>
-    dplyr::left_join(fp_main)
+    dplyr::rename(fp_fish_id = `_child_record_id`, fp_main_id = `_parent_id`) |>
+    dplyr::left_join(dplyr::select(fp_main, fp_main_id, date, trap_check)) |>
+    dplyr::select(date, trap_check, species = species_measure, lifestage = lifestage_measure,
+                  measure_type, fl_mm, fish_viewer_id, mark_status = mark_status_measure,
+                  pit_tag_id, floy_tag_id, fish_condition = fish_condition_measure, stomach_sampled,
+                  disposition, fp_fish_id, fp_main_id)
 }
 
 #' Prepare Fulcrum plus counts table
@@ -62,9 +63,9 @@ prep_fp_fish <- function(fp_fish_raw, fp_main){
 
 prep_fp_plus <- function(fp_plus_raw, fp_main){
   fp_plus_raw |>
-    dplyr::select(fp_plus_id = `_child_record_id`, fp_main_id = `_parent_id`,
-                  species = species_plus, lifestage = lifestage_plus,
+    dplyr::rename(fp_plus_id = `_child_record_id`, fp_main_id = `_parent_id`) |>
+    dplyr::left_join(dplyr::select(fp_main, fp_main_id, date, trap_check)) |>
+    dplyr::select(date, trap_check, species = species_plus, lifestage = lifestage_plus,
                   mark_status = mark_status_plus, fish_condition = fish_condition_plus,
-                  count = plus_count) |>
-    dplyr::left_join(fp_main)
+                  count = plus_count, fp_plus_id, fp_main_id)
 }
--- R/trap_platform.R
+++ R/trap_platform.R
@@ -27,13 +27,14 @@ get_tp <- function(table = c("main", "perf_config", "velocity", "water_quality")
 prep_tp_main <- function(tp_main_raw){
   tp_main_raw |>
     dplyr::rename(tp_main_id = `_record_id`) |>
-    dplyr::select(!starts_with("_") & !contains("notes") & !contains("comments") &
-                    !contains("photos") & !contains("qc")) |>
+    dplyr::select(!dplyr::starts_with("_") & !dplyr::contains("photos")) |>
     dplyr::mutate(shift_start = prep_dt_fulcrum(date, shift_start),
                   shift_end = prep_dt_fulcrum(date, shift_end),
                   shift_hrs = as.numeric(difftime(shift_end, shift_start, units = "hours")),
                   trap_close = prep_dt_fulcrum(date, trap_close),
-                  trap_open = prep_dt_fulcrum(date, trap_open))
+                  trap_open = prep_dt_fulcrum(date, trap_open)) |>
+    dplyr::relocate(shift_hrs, .after = shift_end) |>
+    dplyr::relocate(tp_main_id, .after = dplyr::last_col())
 }
 
 #' Prepare Fulcrum performance and configuration table
@@ -48,10 +49,11 @@ prep_tp_main <- function(tp_main_raw){
 
 prep_tp_perf_config <- function(tp_perf_config_raw, tp_main){
   tp_perf_config_raw |>
-    dplyr::select(tp_perf_config_id = `_child_record_id`, tp_main_id = `_parent_id`,
-                  component, performance, describe_configuration, modified, new_configuration,
-                  fish_impinged, impinged_fish_desc) |>
-    dplyr::left_join(dplyr::select(tp_main, tp_main_id, date, trap_check_number))
+    dplyr::rename(tp_perf_config_id = `_child_record_id`, tp_main_id = `_parent_id`) |>
+    dplyr::select(!dplyr::starts_with("_")) |>
+    dplyr::left_join(dplyr::select(tp_main, tp_main_id, date, trap_check)) |>
+    dplyr::relocate(date, trap_check, .before = tp_perf_config_id) |>
+    dplyr::relocate(tp_perf_config_id, tp_main_id, .after = dplyr::last_col())
 }
 
 #' Prepare Fulcrum velocity table
@@ -69,7 +71,9 @@ prep_tp_velocity <- function(tp_velocity_raw, tp_main){
     dplyr::rename(tp_velocity_id = `_child_record_id`, tp_main_id = `_parent_id`) |>
     dplyr::select(!starts_with("_"))  |>
     dplyr::mutate(velocity_fps = sqrt(u_fps^2 + v_fps^2 + w_fps^2)) |>
-    dplyr::left_join(dplyr::select(tp_main, tp_main_id, date, trap_check_number))
+    dplyr::left_join(dplyr::select(tp_main, tp_main_id, date, trap_check)) |>
+    dplyr::relocate(date, trap_check, .before = tp_velocity_id) |>
+    dplyr::relocate(tp_velocity_id, tp_main_id, .after = dplyr::last_col())
 }
 
 #' Prepare Fulcrum velocity table
@@ -86,5 +90,7 @@ prep_tp_wq <- function(tp_wq_raw, tp_main){
   tp_wq_raw |>
     dplyr::rename(tp_wq_id = `_child_record_id`, tp_main_id = `_parent_id`) |>
     dplyr::select(!starts_with("_")) |>
-    dplyr::left_join(dplyr::select(tp_main, tp_main_id, date, trap_check_number))
+    dplyr::left_join(dplyr::select(tp_main, tp_main_id, date, trap_check)) |>
+    dplyr::relocate(date, trap_check, .before = tp_wq_id) |>
+    dplyr::relocate(tp_wq_id, tp_main_id, .after = dplyr::last_col())
 }
