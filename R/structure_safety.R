f4699e69043b4f6613114ea67d3843599c0d58f8
Travis Hinkelman <thinkelman@esassoc.com>
2024-09-29 21:48:05 -0700

    Expand structure safety fields

--- R/structure_safety.R
+++ R/structure_safety.R
@@ -27,9 +27,12 @@ get_ssi <- function(table = c("main", "debris_boom", "net_anchor", "net_dock", "
 
 prep_ssi_main <- function(ssi_main_raw){
   ssi_main_raw |>
-    dplyr::select(ssi_main_id = `_record_id`, date = date_temp, jscs_site, water_depth_tool,
-                  camera_id, trap_anchor_status, trap_anchor_status_notes,
-                  flow_patterns, modification_notes)
+    dplyr::rename(ssi_main_id = `_record_id`) |>
+    dplyr::select(!dplyr::starts_with("_") & !dplyr::contains("photos")) |>
+    dplyr::mutate(start_time = prep_dt_fulcrum(date, start_time),
+                  end_time = prep_dt_fulcrum(date, end_time)) |>
+    dplyr::relocate(ssi_main_id, .after = dplyr::last_col()) |>
+    dplyr::arrange(desc(date))
 }
 
 #' Prepare Fulcrum debris boom inspection table
@@ -44,11 +47,13 @@ prep_ssi_main <- function(ssi_main_raw){
 
 prep_ssi_debris_boom <- function(ssi_debris_boom_raw, ssi_main){
   ssi_debris_boom_raw |>
-    dplyr::select(ssi_debris_boom_id = `_child_record_id`, ssi_main_id = `_parent_id`,
-                  anchor = debris_boom_anchor, anchor_status = debris_boom_anchor_status,
+    dplyr::rename(ssi_debris_boom_id = `_child_record_id`, ssi_main_id = `_parent_id`) |>
+    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date)) |>
+    dplyr::select(date, anchor = debris_boom_anchor, anchor_status = debris_boom_anchor_status,
                   debris_present = debris_boom_debris_present, debris_details,
-                  debris_removed, debris_removal_details, debris_boom_notes) |>
-    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date))
+                  debris_removed, debris_removal_details, debris_boom_notes,
+                  ssi_debris_boom_id, ssi_main_id) |>
+    dplyr::arrange(desc(date))
 }
 
 #' Prepare Fulcrum guidance net anchor inspection table
@@ -63,10 +68,13 @@ prep_ssi_debris_boom <- function(ssi_debris_boom_raw, ssi_main){
 
 prep_ssi_net_anchor <- function(ssi_net_anchor_raw, ssi_main){
   ssi_net_anchor_raw |>
-    dplyr::select(ssi_net_anchor_id = `_child_record_id`, ssi_main_id = `_parent_id`,
-                  anchor = guidance_net_anchor, anchor_load_lbs,
-                  anchor_status = guidance_net_anchor_status) |>
-    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date))
+    dplyr::rename(ssi_net_anchor_id = `_child_record_id`, ssi_main_id = `_parent_id`) |>
+    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date)) |>
+    dplyr::mutate(datetime = prep_dt_fulcrum(date, anchor_load_time)) |>
+    dplyr::select(date, datetime, anchor = guidance_net_anchor, anchor_load_lbs,
+                  anchor_status = guidance_net_anchor_status,
+                  ssi_net_anchor_id, ssi_main_id) |>
+    dplyr::arrange(desc(date))
 }
 
 #' Prepare Fulcrum guidance net dock inspection table
@@ -81,10 +89,12 @@ prep_ssi_net_anchor <- function(ssi_net_anchor_raw, ssi_main){
 
 prep_ssi_net_dock <- function(ssi_net_dock_raw, ssi_main){
   ssi_net_dock_raw |>
-    dplyr::select(ssi_net_dock_id = `_child_record_id`, ssi_main_id = `_parent_id`,
-                  dock = guidance_net_dock, connection_status,
-                  debris_present = guidance_net_debris_present) |>
-    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date))
+    dplyr::rename(ssi_net_dock_id = `_child_record_id`, ssi_main_id = `_parent_id`) |>
+    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date)) |>
+    dplyr::select(date, dock = guidance_net_dock, connection_status,
+                  debris_present = guidance_net_debris_present,
+                  ssi_net_dock_id, ssi_main_id) |>
+    dplyr::arrange(desc(date))
 }
 
 #' Prepare Fulcrum structure inspection table
@@ -99,9 +109,11 @@ prep_ssi_net_dock <- function(ssi_net_dock_raw, ssi_main){
 
 prep_ssi_structure <- function(ssi_structure_raw, ssi_main){
   ssi_structure_raw |>
-    dplyr::select(ssi_structure_id = `_child_record_id`, ssi_main_id = `_parent_id`,
-                  dock = structure_dock, water_depth_ft, guidance_net_depth_ft,
-                  impermeable_panel_depth_ft, dock_winch_status, dock_modified) |>
-    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date))
+    dplyr::rename(ssi_structure_id = `_child_record_id`, ssi_main_id = `_parent_id`, dock = structure_dock) |>
+    dplyr::select(!dplyr::starts_with("_")) |>
+    dplyr::left_join(dplyr::select(ssi_main, ssi_main_id, date)) |>
+    dplyr::relocate(ssi_main_id, .before = ssi_structure_id) |>
+    dplyr::relocate(ssi_structure_id, ssi_main_id, .after = dplyr::last_col()) |>
+    dplyr::arrange(desc(date))
 }
 
